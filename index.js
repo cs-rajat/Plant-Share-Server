e79f55d58ba88b21ed4b6d426e3254ca1c8922c2
Rahul <cs.rajat75@gmail.com>
2025-11-12 23:50:00 +0600

    index update

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ import connectDB from "./src/config/db.js";
 import foodRoutes from "./src/routes/food.routes.js";
 import requestRoutes from "./src/routes/request.routes.js";
 
+
+
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 connectDB();
-
 app.get("/", (req, res) => res.send("✅ PlateShare Server Running"));
 app.use("/api/foods", foodRoutes);
 app.use("/api/requests", requestRoutes);
